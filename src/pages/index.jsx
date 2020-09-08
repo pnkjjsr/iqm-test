6555dfd01158021d41e62fc58d72c8b62e7da5c0
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2020-09-08 17:46:48 +0530

    feat(ref): moment added for time test with Stack API.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Container from "@material-ui/core/Container";
 import InfiniteScroll from "react-infinite-scroll-component";
+import moment from 'moment';
+
 
 import Layout from "@layouts/open/index";
 import Item from "@components/Item";
@@ -22,12 +24,12 @@ export default function Home({ data }) {
     let no = apiPage + 1;
 
     let res = await fetch(
-      `https://api.stackexchange.com/2.2/questions?page=${no}&pagesize=20&order=desc&sort=activity&tagged=javascript&site=stackoverflow`
-      // `https://newsapi.org/v2/everything?page=${no}&pageSize=20&q=bitcoin&from=2020-08-07&sortBy=publishedAt&apiKey=56f97ce62cde4666882c52ca9e986f58`
+      `https://api.stackexchange.com/2.2/questions?page=${no}&pagesize=50&order=desc&sort=activity&tagged=javascript&site=stackoverflow`
+      // `https://newsapi.org/v2/everything?page=${no}&pageSize=20&q=bitcoin&from=2020-08-08&sortBy=publishedAt&apiKey=56f97ce62cde4666882c52ca9e986f58`
     );
     let data = await res.json();
 
-    setItems(Object.values(data.items).concat());
+    setItems(items.concat(Object.values(data.items)));
     // setItems(items.concat(Object.values(data.articles)));
   };
 
@@ -58,18 +60,13 @@ export default function Home({ data }) {
               }
             >
               {items.map((i, index) => {
-                let iso = i.creation_date.setSeconds(0, 0);
-                let date = new Date(iso.toISOString);
-                let year = date.getFullYear();
-                let month = date.getMonth() + 1;
-                let dt = date.getDate();
-
+                let time = moment.unix(i.creation_date, "x").format("LLL");
                 return (
                   <Item
                     key={index}
                     title={i.title}
-                    author={i.author}
-                    date={`${dt}/${month}/${year}`}
+                    author={i.owner.display_name}
+                    date={time}
                     data={i}
                   />
                 );
@@ -84,8 +81,8 @@ export default function Home({ data }) {
 
 export async function getServerSideProps() {
   const res = await fetch(
-    "https://api.stackexchange.com/2.2/questions?page=1&pagesize=20&order=desc&sort=activity&tagged=javascript&site=stackoverflow"
-    // "https://newsapi.org/v2/everything?page=1&pageSize=20&q=bitcoin&from=2020-08-07&sortBy=publishedAt&apiKey=56f97ce62cde4666882c52ca9e986f58"
+    "https://api.stackexchange.com/2.2/questions?page=1&pagesize=50&order=desc&sort=activity&tagged=javascript&site=stackoverflow"
+    // "https://newsapi.org/v2/everything?page=1&pageSize=20&q=bitcoin&from=2020-08-08&sortBy=publishedAt&apiKey=56f97ce62cde4666882c52ca9e986f58"
   );
   const data = await res.json();
 
